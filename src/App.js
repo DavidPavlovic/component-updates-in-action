5bcb949664375e7528494835caa3714fe19f2d88
David <davidpavlovicdev@gmail.com>
2021-12-16 21:01:47 +0100

    convert space to tabs, add button toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    console.log('app running!')
+    
+    const toggleParagraph = () => {
+        setShowParagraph(prevShowparagraph => !prevShowparagraph);
+    }
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {showParagraph && <p>This is new</p>}
+            <Button onClick={toggleParagraph}>Toggle Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
