aaff7d580316be7bb7f8b462ecf97f1677cd9adc
David <davidpavlovicdev@gmail.com>
2022-01-11 10:20:49 +0100

    add dependencie for useCallback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
     const [showParagraph, setShowParagraph] = useState(false);
+    const [allowToggle, setAllowToggle] = useState(false);
 
     console.log('app running!')
 
-    const toggleParagraph = () => {
-        setShowParagraph(prevShowparagraph => !prevShowparagraph);
-    }
+    const toggleParagraph = useCallback(() => {
+        if(allowToggle) setShowParagraph(prevShowparagraph => !prevShowparagraph);
+    }, [allowToggle]);
+
+    const allowToggleHandler = () => {
+        setAllowToggle(true)
+    };
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={false}/>
+            <DemoOutput show={showParagraph}/>
+            <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
             <Button onClick={toggleParagraph}>Toggle Paragraph!</Button>
         </div>
     );
